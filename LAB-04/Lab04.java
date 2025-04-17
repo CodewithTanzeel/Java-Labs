cb634a7c872edac6537bd38d62e9cf5dff1493c8
TanzeelAhmad <Tanzeelofficial@outlook.com>
2025-04-17 13:17:32 +0500

    .

---
+++ LAB-04/Lab04.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class Lab04 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Your Choice: ");
+        int choice = input.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println("Enter Number 1");
+                int num1 = input.nextInt();
+                System.out.println("Enter Number 2");
+                int num2 = input.nextInt();
+                System.out.println("Addtion of both numbers:" + (num1 + num2));
+            case 2:
+                System.out.println("Enter Base");
+                int Base = input.nextInt();
+                System.out.println("Enter exponent");
+                int exp = input.nextInt();
+                System.out.println(Math.pow(Base, exp));
+            case 3:
+                break;
+
+        }
+
+    }
+}
