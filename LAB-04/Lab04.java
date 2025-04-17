1e5274c8fb22145e06fd42a3e565e180256511c6
TanzeelAhmad <Tanzeelofficial@outlook.com>
2025-04-17 23:19:12 +0500

    .

--- LAB-04/Lab04.java
+++ LAB-04/Lab04.java
@@ -7,23 +7,68 @@ public class Lab04 {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter Your Choice: ");
         int choice = input.nextInt();
-        switch (choice) {
-            case 1:
-                System.out.println("Enter Number 1");
-                int num1 = input.nextInt();
-                System.out.println("Enter Number 2");
-                int num2 = input.nextInt();
-                System.out.println("Addtion of both numbers:" + (num1 + num2));
-            case 2:
-                System.out.println("Enter Base");
-                int Base = input.nextInt();
-                System.out.println("Enter exponent");
-                int exp = input.nextInt();
-                System.out.println(Math.pow(Base, exp));
-            case 3:
-                break;
+        // switch (choice) {
+        //     case 1:
+        //         System.out.println("Enter Number 1");
+        //         int num1 = input.nextInt();
+        //         System.out.println("Enter Number 2");
+        //         int num2 = input.nextInt();
+        //         System.out.println("Addtion of both numbers:" + (num1 + num2));
+        //     case 2:
+        //         System.out.println("Enter Base");
+        //         int Base = input.nextInt();
+        //         System.out.println("Enter exponent");
+        //         int exp = input.nextInt();
+        //         System.out.println(Math.pow(Base, exp));
+        //     case 3:
+        //         break;
+
+        // }
+        // switch(choice){
+        //     case 1:
+        //     System.out.println("Enter the amount spended on Electronices");
+        //     int ElectronicsAmount = input.nextInt();
+        //   System.out.println("The Amount u spended on Electronics is:"+ElectronicsAmount);
+        
+        //     case 2:
+        //     System.out.println("Enter the amount spended on Grocieries");
+        //     int GroceriesAmount = input.nextInt();
+        //     System.out.println("The Amount u spended on Groceries is:"+GroceriesAmount);
+
+
+        
+        //     case 3:
+        //     System.out.println("Enter the amount spended on Clothing");
+        //     int ClothingAmount = input.nextInt();
+        //     System.out.println("The Amount u spended on Clothing is:"+ClothingAmount);
+
+        
+
+
+
+        //     double SumOfAllAmount = ElectronicsAmount+GroceriesAmount+ClothingAmount;
+        //     System.out.println(SumofAllAmount);
+
+        switch(choice){
+            case 1: 
+                
 
         }
+
+
+
+
+
+
+
+
+
+
+        
+
+    
+
+        
 
     }
 }
