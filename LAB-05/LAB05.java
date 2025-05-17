ad7c0a066ee223e1cff7090bda03cea3b53f3002
TanzeelAhmad <Tanzeelofficial@outlook.com>
2025-05-17 18:12:41 +0500

    .

--- LAB-05/LAB05.java
+++ LAB-05/LAB05.java
@@ -2,19 +2,29 @@ import java.util.Scanner;
 
 
 public class LAB05{
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter your character: ");
-        String y = input.next();
-        
-        String x = "Ali";
-        for(int i = 0; i > x.lenght; i++){
-            if(y == x[i]){
-                System.out.println("Character is found at index x[i]");
+        String y = input.nextLine();
 
-            }else{
-                System.out.println("Character is not Found");
+        if (y.length() != 1) {
+            System.out.println("Please enter exactly one character.");
+            return;
+        }
+        char searchChar = y.charAt(0);
+
+        String x = "Ali";
+        boolean found = false;
+
+        for (int i = 0; i < x.length(); i++) {
+            if (searchChar == x.charAt(i)) {
+                System.out.println("Character '" + searchChar + "' is found at index " + i);
+                found = true;
+                break;
             }
         }
-    }
+
+        if (!found) {
+            System.out.println("Character is not Found");
+        }
 }
