33dc9819a2144bef869ae252a9228d795ffb9b51
TanzeelAhmad <Tanzeelofficial@outlook.com>
2025-05-17 23:02:17 +0500

    .

--- LAB-05/LAB05.java
+++ LAB-05/LAB05.java
@@ -2,29 +2,53 @@ import java.util.Scanner;
 
 
 public class LAB05{
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter your character: ");
-        String y = input.nextLine();
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         System.out.println("Enter your character: ");
+//         String y = input.nextLine();
 
-        if (y.length() != 1) {
-            System.out.println("Please enter exactly one character.");
-            return;
-        }
-        char searchChar = y.charAt(0);
+//         if (y.length() != 1) {
+//             System.out.println("Please enter exactly one character.");
+//             return;
+//         }
+//         char searchChar = y.charAt(0);
 
-        String x = "Ali";
-        boolean found = false;
+//         String x = "Ali";
+//         boolean found = false;
 
-        for (int i = 0; i < x.length(); i++) {
-            if (searchChar == x.charAt(i)) {
-                System.out.println("Character '" + searchChar + "' is found at index " + i);
-                found = true;
+//         for (int i = 0; i < x.length(); i++) {
+//             if (searchChar == x.charAt(i)) {
+//                 System.out.println("Character '" + searchChar + "' is found at index " + i);
+//                 found = true;
+//                 break;
+//             }
+//         }
+
+//         if (!found) {
+//             System.out.println("Character is not Found");
+//         }
+
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter a word: ");
+    String word = scanner.nextLine();
+    
+    for (int i = 0; i < word.length(); i++) {
+        char current = word.charAt(i);
+        boolean unique = true;
+        
+        for (int j = 0; j < word.length(); j++) {
+            if (i != j && current == word.charAt(j)) {
+                unique = false;
                 break;
             }
         }
-
-        if (!found) {
-            System.out.println("Character is not Found");
+        
+        if (unique) {
+            System.out.println("First unique character: " + current);
+            return;
         }
+    }
+    System.out.println("All characters repeat");
+}
 }
